e1d513ee1cbaaf3e63b4e08e66cb698cffbbac29
Alex <alex@alexseifert.com>
2016-06-04 18:03:39 +0200

    The user can now choose which folder he/she would like to add a new feed to

--- src/AppBundle/Controller/FeedsController.php
+++ src/AppBundle/Controller/FeedsController.php
@@ -8,9 +8,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 use AppBundle\Entity\Feed;
+use AppBundle\Entity\Folder;
 
 class FeedsController extends Controller
 {
@@ -19,6 +21,20 @@ class FeedsController extends Controller
      */
     public function getAddFeedForm(Request $request)
     {
+        $translator = $this->get('translator');
+
+        $queryResult = $this->getDoctrine()
+            ->getRepository('AppBundle:Folder')
+            ->findBy(array(), array('folderName' => 'ASC'));
+
+        $folderDropdown = array(
+            '0' => $translator->trans('all_feeds')
+        );
+
+        foreach ($queryResult as $result) {
+            $folderDropdown[$result->getId()] = $result->getFolderName();
+        }
+
         $date = new \DateTime("now");
 
         $feed = new Feed();
@@ -28,9 +44,10 @@ class FeedsController extends Controller
         $form = $this->createFormBuilder($feed)
             ->setAction($this->generateUrl('feedsAddFeedForm'), array('locale' => $request->getLocale()))
             ->setMethod('POST')
-            ->add('feed_name', TextType::class, array('label' => $this->get('translator')->trans('add_new_feed_name')))
-            ->add('feed_url', UrlType::class, array('label' => $this->get('translator')->trans('add_new_feed_url')))
-            ->add('save', SubmitType::class, array('label' => $this->get('translator')->trans('add_new_feed')))
+            ->add('feed_name', TextType::class, array('label' => $translator->trans('add_new_feed_name')))
+            ->add('feed_url', UrlType::class, array('label' => $translator->trans('add_new_feed_url')))
+            ->add('folder', ChoiceType::class, array('label' => $translator->trans('add_folder_name'), 'choices' => $folderDropdown))
+            ->add('save', SubmitType::class, array('label' => $translator->trans('add_new_feed')))
             ->getForm();
 
         $form->handleRequest($request);
