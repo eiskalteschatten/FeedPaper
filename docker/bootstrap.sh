0be66aa16b970bd18e64f5342a745b7ceef3ce66
Alex Seifert <alex@alexseifert.com>
2016-08-17 12:25:48 +0200

    Continued adding Docker support

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -5,10 +5,7 @@ service php7.0-fpm start
 php /var/composer.phar install
 wait
 
-#php app/console doctrine:database:create
-#wait
-
-#php app/console doctrine:schema:update --force
-#wait
+php app/console doctrine:schema:update --force
+wait
 
 tail -f /dev/null
