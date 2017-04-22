475ca58e7668663a24b0b19e9a06b3bb31849ba3
Alex <alex@alexseifert.com>
2017-04-22 16:24:25 +0200

    Updated bootstrap script

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -8,4 +8,13 @@ wait
 php app/console doctrine:schema:update --force
 wait
 
-tail -f /var/www/feedpaper/app/logs/dev.log
+ln -sf ${PWD}/vendor/twbs/bootstrap web/resources/bootstrap
+wait
+
+php app/console assets:install --symlink web
+wait
+
+php app/console server:start 0.0.0.0:8000 --force
+wait
+
+tail -f /var/www/symfony/app/logs/dev.log
